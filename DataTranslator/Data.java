da3d7f620d2a17cde6fa7ce785167098755b90ca
Meegi <manuelmiggy@gmail.com>
2024-03-10 01:36:36 +0800

    datatypes (string, int, long) are now supported to be translated to hexcodes

--- DataTranslator/Data.java
+++ DataTranslator/Data.java
@@ -6,11 +6,11 @@ package DataTranslator;
  */
 
 public class Data {
-	final Object[] SUPPORTED_CLASS = 
+	final static Object[] SUPPORTED_CLASS = 
 		{String.class, Integer.class, Long.class};
-	int classIndex = -1;
+	static int classIndex = -1;
 	
-	public String toBinary(Object o) throws Exception {
+	public static String toBinary(Object o) throws Exception {
 		String binaryString = "";
 		Object classType = o.getClass();
 		int i = 0;
@@ -42,24 +42,48 @@ public class Data {
 		return binaryString;
 	}
 
-	private String readString(String s) {
+	private static String readString(String s) {
         StringBuilder result = new StringBuilder();
         char[] chars = s.toCharArray();
         for (char aChar : chars) {
             result.append(
-                    String.format("%8s", Integer.toBinaryString(aChar))   // char -> int, auto-cast
-                            .replaceAll(" ", "0")                         // zero pads
+            	String.format("%8s", Integer.toBinaryString(aChar))
+            	.replaceAll(" ", "0")
             );
         }
         return result.toString();
 	}
 
-	private String readInt(Integer o) {
-		return null;
+	private static String readInt(Integer i) {
+		StringBuilder result = new StringBuilder();
+		
+		while (i > 0) {
+			if (i % 2 == 0)
+				result.append('0');
+			else
+				result.append('1');
+			i /= 2;
+		}
+		
+		return result.reverse().toString();
 	}
 	
-	private String readLong(Long o) {
-		return null;
+	private static String readLong(Long l) {
+		/**
+		 * as of now same purpose lang sila with int but chances are...
+		 * actually idk why i made a long version whoops
+		 */
+		StringBuilder result = new StringBuilder();
+		
+		while (l > 0) {
+			if (l % 2 == 0)
+				result.append('0');
+			else
+				result.append('1');
+			l /= 2;
+		}
+		
+		return result.reverse().toString();
 	}
 
 }
--- DataTranslator/Translator.java
+++ DataTranslator/Translator.java
@@ -1,5 +1,7 @@
 package DataTranslator;
 
+import java.util.ArrayList;
+
 /**
  * 
  * purpose neto is to translate a given binary string (LITERAL STRING!)
@@ -8,7 +10,35 @@ package DataTranslator;
  */
 
 public class Translator {
-	public long dataToBinary(Object o){
-		return -1;
+	public int[] toHex(Object o) throws Exception{
+		String b = Data.toBinary(o);
+		ArrayList<String> binaryArr = new ArrayList<String>();
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		String temp = null;
+		
+		do{
+			try {
+				temp = b.substring(0, 8);
+				binaryArr.add(temp);
+				b = b.substring(8);
+			}
+			catch(Exception x) {
+				b = compensate(b);
+			}
+		}
+		while(!b.equals("00000000"));
+		
+		for (String s : binaryArr) {
+			result.add(Integer.parseInt(s, 2));
+		}
+		
+		return result.stream().mapToInt(i -> i).toArray();
+	}
+	
+	private String compensate(String b) { // my pp is not smol...
+		while(b.length() < 8) {
+			b += '0';
+		}
+		return b;
 	}
 }
