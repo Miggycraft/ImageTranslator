cf28797978c2e6e2eea23bff39e0f3f2f8f07a8b
Meegi <manuelmiggy@gmail.com>
2024-03-17 10:27:13 +0800

    simplified query and now adds feedback if success

--- Database/ImageQuery.java
+++ Database/ImageQuery.java
@@ -7,6 +7,7 @@ import ImageDrawer.TableImage;
 
 public class ImageQuery {
 	private ImageBase ib;
+	private String tableName;
 	
 	public void setDimension(int x, int y) {
 		ib.setDimension(x, y); // lol
@@ -16,13 +17,35 @@ public class ImageQuery {
 		this.ib = ib;
 	}
 	
+	public void write(String s) {
+		write(s, tableName);
+	}
+	
 	public void write(String s, String tableName) {
 		TableImage ti = ib.findOrMake(tableName);
+		printSuccess(s);
 		ti.paintString(s);
+	}
+	
+	public ArrayList<String> read(){
+		return read(tableName);
 	}
 	
 	public ArrayList<String> read(String tableName) {
 		TableImage ti = ib.findOrMake(tableName);
 		return ti.read();
+	}
+	
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+	
+	public void printSuccess(String s) {
+		if (s.length() > 10) {
+			System.out.println("Message: " + s.substring(0, 10) + "... is written successfully!");
+		}
+		else {
+			System.out.println("Message: " + s + " is written successfully!");
+		}
 	}
 }
